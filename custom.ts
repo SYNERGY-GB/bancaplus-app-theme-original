d349c543df235def0cfaea08b0ca913d22c8f84e
emmanueldeaguiar <emmanuel.deaguiar@gmail.com>
2017-05-19 16:18:12 -0400

    README and custom.ts

---
+++ custom.ts
@@ -0,0 +1,10 @@
+export var customize = (metadata : Megazord.ContainerScreenList) : void => {
+  //Default customization of metadata (does nothing).
+  metadata['dashboard.dashboard']['params']['modalAnimation'] = 'scale-out';
+
+
+  //customize.json
+  
+}
+
+//End of custom.ts
